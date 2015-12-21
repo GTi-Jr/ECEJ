b53f6b5475ded14e24a70b408a9f3272518ed38e
Rodrigo Teixeira <rodrigotnoronha@gmail.com>
2015-12-21 16:34:20 -0300

    Add step registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,15 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
-         
-  has_one :adress
+
+  has_one :address
   has_many :special_needs, -> { where special_needs: true }
-  
+
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   def is_completed
-    if self.name == nil || self.cpf == nil || self.general_register == nil 
+    if self.completed == nil || self.completed == false
     	return false
     else
     	return true
