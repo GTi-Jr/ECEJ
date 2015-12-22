dc3edcd170b0e09d4db49fcf914b7b677abaac28
CaioEPS <caioergos@gmail.com>
2015-12-22 01:47:21 -0300

    lot added to users +new User methods +constants

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,12 +15,52 @@ class User < ActiveRecord::Base
     true
   end
 
+  # Distribuição de lotes
+  # Lote 0: Fila de espera
+  # Lote 1: 50
+  # Lote 2: 150
+  # lote 3: 100
+  
+  def self.first_lot
+    User.where(lot: 1).order("created_at")
+  end
+
+  def self.second_lot
+    User.where(lot: 2).order("created_at")
+  end
+
+  def self.third_lot
+    User.where(lot: 3).order("created_at")
+  end
+
+  def self.waiting_list 
+    User.where(lot: 0).order("created_at")
+  end
+
+  # Insert users into lists
+  def insert_into_waiting_list
+    self.update_attribute(:lot, 0)
+  end
+
+  def insert_into_first_lot
+    self.update_attribute(:lot, 1)
+  end
+
+  def insert_into_second_lot
+    self.update_attribute(:lot, 2)
+  end
+
+  def insert_into_third_lot
+    self.update_attribute(:lot, 3)
+  end
+
+
   # checks if the user has confirmed within 15 days.
   def has_passed_15_days_since_creation?
     (Time.now - self.created_at) > 15.days
   end
 
-  # Scheduled task to inactivate users
+  # Scheduled task to inactivate users.
   def self.delete_inactive_users!
     User.all.each do |user|
       unless user.is_completed? && user.has_paid?
@@ -28,6 +68,15 @@ class User < ActiveRecord::Base
         Rails.logger.info "--- RETIRADO POR INATIVIDADE: #{user.name}" unless user.active
       end
     end
+  end
+
+  # List of those people who's confirmed into the main event.
+  def self.priority_list
+    User.where(active: true)
+  end
+
+  def self.insert_into_priority_list
+    
   end
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151221232608) do
+ActiveRecord::Schema.define(version: 20151222041903) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer  "user_id"
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 20151221232608) do
     t.boolean  "completed"
     t.text     "special_needs"
     t.boolean  "active"
+    t.integer  "lot"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
