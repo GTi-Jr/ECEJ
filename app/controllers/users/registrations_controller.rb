1cfbb573e3d5fe65ae3e73b1d453f6bfe25db831
Rodrigo Teixeira <rodrigotnoronha@gmail.com>
2015-12-21 22:33:04 -0300

    After Registration controller configurado e iniciando configuração de views

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -12,24 +12,24 @@ before_filter :configure_account_update_params, only: [:update]
     super
   end
 
-  #GET /user/edit
-  def edit
-    super do |resource|
-    #Here you add what you'll do AFTER devise works
-    end
-  end
-
-  #PUT /user
-  def update
-    @user = User.find(current_user.id)
-    if @user.update(user_params)
-      # Sign in the user by passing validation in case their password changed
-      sign_in @user, :bypass => true
-      redirect_to root_path
-    else
-      render "edit"
-    end
-  end
+  # #GET /user/edit
+  # def edit
+  #   super do |resource|
+  #   #Here you add what you'll do AFTER devise works
+  #   end
+  # end
+  #
+  # #PUT /user
+  # def update
+  #   @user = User.find(current_user.id)
+  #   if @user.update(user_params)
+  #     # Sign in the user by passing validation in case their password changed
+  #     sign_in @user, :bypass => true
+  #     redirect_to root_path
+  #   else
+  #     render "edit"
+  #   end
+  # end''
 
   #DELETE /user
   def destroy
