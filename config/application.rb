378dcb62a5a9909ea9d705dae45671ef93db96be
rodrigotnoronha <rodrigo_tn@live.com>
2015-12-23 00:41:14 -0300

    Added template and init login view configuration

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ module Ecej2016
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'America/Sao_Paulo'
 
+    config.assets.precompile =  [ '*.js', '*.scss']
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
