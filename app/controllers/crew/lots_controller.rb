81f10799768e07da5eadb57bbb7e9e7b7919a818
CaioEPS <caioergos@gmail.com>
2015-12-25 02:31:21 -0300

    lots controller

---
+++ app/controllers/crew/lots_controller.rb
@@ -0,0 +1,43 @@
+class Crew::LotsController < ApplicationController
+  before_action :authenticate_crew_admin!
+  before_action :load_lot, only: [:edit, :update]
+
+  def index
+    @lots = Lot.all
+  end
+
+  def new
+    @lot = Lot.new    
+  end
+
+  def create
+    @lot = Lot.new(lot_params)
+
+    if @lot.save
+      redirect_to crew_lot_index_path, notice: "Lote criado com sucesso"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @lot.update_attributes(lot_params)
+      redirect_to crew_lot_edit_path(@lot), notice: "Lote editado com sucesso."
+    else
+      redirect_to crew_lot_edit_path(@lot), alert: "Não foi possível alterar lote."
+    end
+  end
+
+  private
+  def load_lot
+    @lot = Lot.find(params[:id])    
+  end
+
+  def lot_params
+    params.require(:lot).permit(:name, :number, :limit, :value_federated, 
+                                :value_not_federated, :start_date, :end_date)
+  end
+end
