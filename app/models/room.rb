ec722eb090bfab3e22d84c291093f4559ba94d21
CaioEPS <caioergos@gmail.com>
2015-12-26 14:47:43 -0300

    room validations

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,9 @@
 class Room < ActiveRecord::Base
+  validate :hotel,
+            exclusion: { in: %w(Jangadeiro Donana) }
+  validate :number,
+           uniqueness: { scope: :hotel },
+           numericality: { greater_than: 0 }
+  validate :number,
+           numericality: { greater_than: 0 }
 end
