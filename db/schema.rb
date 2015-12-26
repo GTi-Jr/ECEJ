ffe39bb23d996b1034c73e3ceda955d36a1beafc
CaioEPS <caioergos@gmail.com>
2015-12-26 14:32:40 -0300

    icons change in lots/_form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151223043219) do
+ActiveRecord::Schema.define(version: 20151226165543) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer  "user_id"
