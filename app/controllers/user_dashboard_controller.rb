9b80ed37308526fb15d8e6b85809349d1072e46d
Rodrigo Teixeira <rodrigotnoronha@gmail.com>
2015-12-25 21:19:06 -0300

    Change color and user dashboard

--- app/controllers/user_dashboard_controller.rb
+++ app/controllers/user_dashboard_controller.rb
@@ -2,7 +2,6 @@ class UserDashboardController < ApplicationController
   before_action :authenticate_user!
   before_action :get_user
   before_action :verify_register_conclusion
-  layout 'user_profile'
 
   def index
   end
