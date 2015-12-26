e5a9003806c5a03b5bb5eadae7a0ba7fd498c0ba
CaioEPS <caioergos@gmail.com>
2015-12-26 00:09:38 -0300

    login route for admin

--- app/controllers/crew/sessions_controller.rb
+++ app/controllers/crew/sessions_controller.rb
@@ -17,7 +17,11 @@ class Crew::SessionsController < Devise::SessionsController
     super
   end
 
-  # protected
+  protected
+  #The path used after sign up.
+  def after_sign_in_path_for(admin)
+    crew_dashboard_path
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_in_params
