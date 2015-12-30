d64eeec843404e2d0385ff9c76b1b80636e2ea0b
Rodrigo Teixeira <rodrigotnoronha@gmail.com>
2015-12-30 02:44:13 -0300

    Tested pagseguro checkout - dashboard e checkout views reescritas

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -3,34 +3,50 @@ class CheckoutController < ApplicationController
   before_action :get_user
   before_action :verify_register_conclusion
 
+  layout "user_dashboard"
+
   def new
-    order(@user)
+    #order(@user)
     if @total == 0
       redirect_to root_path, :alert => "Você ainda não tem itens para ser comprados"
     end
-
-    session[:price] = (@total + @total/20)
+    if(@user.federation == nil)
+      @total = 15
+    else
+      @total = 10
+    end
+    session[:price] = @total
+    session[:description] = 'Teste'
   end
 
   def create
     payment = PagSeguro::PaymentRequest.new
 
-    payment.reference = order.id
-    payment.notification_url = notifications_url
-    payment.redirect_url = processing_url
+    payment.reference = 'lote[id]user[id]'
+    # payment.notification_url = notifications_url
+    payment.redirect_url = 'localhost:3000/payment'
 
       payment.items << {
         id: 1,
-        description: product.title,
-        amount: product.price
+        description: session[:description],
+        amount: session[:price]
+      }
+
+      payment.sender = {
+        name: @user.name,
+        email: 'c14330875633327824357@sandbox.pagseguro.com.br',
+        cpf: '06105889313',
+        phone: {
+          number: '88310483'
+        }
       }
 
     # Caso você precise passar parâmetros para a api que ainda não foram
     # mapeados na gem, você pode fazer de maneira dinâmica utilizando um
     # simples hash.
-    payment.extra_params << { paramName: 'paramValue' }
-    payment.extra_params << { senderBirthDate: '07/05/1981' }
-    payment.extra_params << { extraAmount: '-15.00' }
+    # payment.extra_params << { paramName: 'paramValue' }
+    # payment.extra_params << { senderBirthDate: '07/05/1981' }
+    # payment.extra_params << { extraAmount: '-15.00' }
 
     response = payment.register
 
