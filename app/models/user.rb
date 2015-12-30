e7ad08ad6f1bac673080e07e56cd0f2d5035882e
CaioEPS <caioergos@gmail.com>
2015-12-30 02:23:02 -0300

    FIX: Lot can now be edited

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,9 @@ class User < ActiveRecord::Base
     User.where(lot_id: nil, active: nil).order(:created_at)
   end
   def self.eligible
-    User.where(completed: true).order(:created_at)
+    User.where(active: true, completed: true, lot_id: nil).order(:created_at).select do |user|
+      !user.paid_on.nil?
+    end
   end
   def self.allocated
     User.all.order(:created_at).select { |user| user.lot_id.is_a? Integer }
@@ -34,30 +36,25 @@ class User < ActiveRecord::Base
   def insert_into_waiting_list!
     self.update_attribute(:lot_id, nil)
   end
-  def insert_into_active_lot!
-    self.update_attribute(:lot_id, Lot.active_lot.id) unless Lot.active_lot.nil?
+  def insert_into_final_lot!
+    self.update_attribute(:lot_id, 3) unless Lot.find(3).nil?
   end
 
   # METHODS USED IN THE SCHEDULED TASK
   # Call this method in the scheduled task
   def self.organize_lots!
-    final_lot = Lot.find(3)
+    final_lot = Lot.find(3) unless Lot.find(3).nil?
     # Take those user in the waiting list who hasn't completed
     User.insert_inactive_users_into_disqualified_lot! 
     Lot.remove_overdue_users!
 
-    User.eligible.each do |user|
-      if Lot.active_lot.users.count <= Lot.active_lot.limit && user.has_paid_in_time?
-        user.insert_into_active_lot!
-      else
-        user.insert_into_waiting_list
+    unless User.eligible.nil?
+      User.eligible.each do |user|
+        if final_lot.users.count <= final_lot.limit && user.has_paid_in_time?
+          user.insert_into_final_lot!
+        end
       end
     end
-  end
-
-  # checks if the user has confirmed within 15 days.
-  def has_passed_15_days_since_creation?
-    (Time.now - self.created_at) > 15.days
   end
 
   # This checking is possible since paid_on's default value is nil.
@@ -75,18 +72,6 @@ class User < ActiveRecord::Base
       end
     else
       return false
-    end
-  end
-
-  # Method to inactivate users who hasn't qualified in 15 days.
-  def self.insert_inactive_users_into_disqualified_lot!
-    User.waiting_list.each do |user|
-      unless user.is_completed? && user.has_paid?
-        if user.has_passed_15_days_since_creation?
-          user.insert_into_disqualified_list!
-          Rails.logger.info "--- RETIRADO POR INATIVIDADE: #{user.name}"
-        end
-      end
     end
   end
 
--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -1,8 +1,33 @@
 require 'rufus-scheduler'
 
-s = Rufus::Scheduler.singleton
+singleton = Rufus::Scheduler.singleton
+scheduler = Rufus::Scheduler.new
 
-s.every '4h' do
+singleton.every '4h' do
   Rails.logger.info "Running user verifications on #{Time.now}"
   User.organize_lots!
+end
+
+day_before_2 = Lot.find(2).start_date - 1.day
+year_2 = day_before_2.year
+month_2 = day_before_2.month
+day_2 = day_before_2.day
+hour_2 = day_before_2.hour
+minute_2 = day_before_2.minute
+second_2 = day_before_2.second
+
+scheduler.at "#{year_2}/#{month_2}/#{day_2} #{hour_2}:#{minute_2}:#{second_2}" do
+  #User.send_lot_2_antecipated_emails
+end
+
+day_before_3 = Lot.find(3).start_date - 1.day
+year_3 = day_before_3.year
+month_3 = day_before_3.month
+day_3 = day_before_3.day
+hour_3 = day_before_3.hour
+minute_3 = day_before_3.minute
+second_3 = day_before_3.second
+
+scheduler.at "#{year_3}/#{month_3}/#{day_3} #{hour_3}:#{minute_3}:#{second_3}" do
+  #User.send_lot_3_antecipated_emails
 end
