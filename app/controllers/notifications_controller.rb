84aef8481ae07275832e6468a21282f783f3581e
CaioEPS <caioergos@gmail.com>
2016-01-09 16:43:19 -0300

    CRSF null if json or xml

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,6 @@
 class NotificationsController < ApplicationController
-  protect_from_forgery with: :null_session
-  
+  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' || c.request.format == 'application/xml'}
+
   def confirm_payment
     transaction = PagSeguro::Transaction.find_by_notification_code(params[:notificationCode])
     if transaction.errors.empty?
@@ -8,5 +8,8 @@ class NotificationsController < ApplicationController
       pagseg_notification.log = transaction.to_yaml
       pagseg_notification.save
     end
+    pagseg_notification = PagSeguroNotification.new
+    pagseg_notification.log = transaction.to_yaml
+    pagseg_notification.save
   end
 end
