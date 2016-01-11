a1f03c6f28f4d77b4d556a5e3e4a14b998e56969
CaioEPS <caioergos@gmail.com>
2016-01-11 11:29:02 -0300

    payments controller

---
+++ app/controllers/crew/payments_controller.rb
@@ -0,0 +1,30 @@
+class Crew::PaymentsController < ApplicationController
+  before_action :authenticate_crew_admin!
+  before_action :load_user
+
+  def set_user_payment
+    status = params[:payment_status]
+
+    case status
+    when 'paid'
+      @user.payment_status = "Pago"
+    when 'waiting'
+      @user.payment_status = "Em processamento"
+    when 'cancelled'   
+      @user.payment_status = "Não processado"
+    when 'non_paid'
+      @user.payment_status = "Não processado"   
+    end
+
+    if @user.save
+      redirect to :back, notice: "Stats do pagamentoa foi alterado."
+    else
+      redirect_to :back, alert: "Não foi possível alterar o status do pagamento."
+    end
+  end
+
+  private
+    def load_user    
+      @user = User.find(params[:id])
+    end
+end
