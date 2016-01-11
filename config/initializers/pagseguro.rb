b203fbb5fa7f2dad05f1c0ef665feebd47f6c07d
Rodrigo Teixeira <rodrigotnoronha@gmail.com>
2016-01-11 02:13:27 -0300

    Edit pagseguro config

--- config/initializers/pagseguro.rb
+++ config/initializers/pagseguro.rb
@@ -1,6 +1,10 @@
 PagSeguro.configure do |config|
   config.token       = Rails.application.secrets.pag_seguro_token
   config.email       = Rails.application.secrets.pag_seguro_email
-  config.environment = :sandbox # ou :sandbox. O padrão é production.
+  if Rails.env.productio?
+    config.environment = :production
+  else
+    config.environment = :sandbox # ou :sandbox. O padrão é production.
+  end
   config.encoding    = "UTF-8" # ou ISO-8859-1. O padrão é UTF-8.
 end
