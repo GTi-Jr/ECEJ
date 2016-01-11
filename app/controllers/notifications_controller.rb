760cb5a922ce984a0b1b537180bbed1572a3fbaf
CaioEPS <caioergos@gmail.com>
2016-01-11 03:51:11 -0300

    notifications controller changes

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -5,14 +5,42 @@ class NotificationsController < ApplicationController
     Rails.logger.info "NOTIFICAÇÃO RECEBIDA #{params[:notificationCode]}"
     transaction = PagSeguro::Transaction.find_by_notification_code(params[:notificationCode])
     if transaction.errors.empty?
-      Rails.logger.info " NOTIFICAÇÃO ENCONTRADA"
-      Rails.logger.info "    Enviada por #{t.sender.email}"
+      Rails.logger.info "\n\n\n NOTIFICAÇÃO ENCONTRADA"
+      Rails.logger.info "\n\n  Enviada por #{transaction.sender.email}"
+      Rails.logger.info "\n\n  Status: #{transaction.status}"
       user = User.where(email: transaction.sender.email).first
-      user.payment_status = "Pago"
-      user.paid_on = DateTime.now
+      case transaction.status.status
+      when :initiated
+        user.payment_status = "Em processamento"
+      when :waiting_payment
+        user.payment_status = "Em processamento"
+      when :in_analysis
+        user.payment_status = "Em processamento"
+      when :paid
+        user.payment_status = "Pago"
+        user.paid_on ||= Datetime.now
+      when :avaliable
+        user.payment_status = "Pago"
+        user.paid_on ||= DateTime.now
+      when :in_dispute
+      when :refunded
+        user.payment_status = "Não processado"
+        user.paid_on = nil
+        user.lot = nil
+      when :cancelled
+        user.payment_status = "Não processado"
+        user.paid_on = nil
+        user.lot = nil
+      when :chargeback_charged
+        user.payment_status = "Não processado"
+        user.paid_on = nil
+        user.lot = nil
+      when :contested
+        user.payment_status = "Não processado"
+      end
       user.save
     else
-      Rails.logger.info "Erros ao receber notificação:"
+      Rails.logger.info "\n\n\n   Erros ao receber notificação:"
       transaction.errors.to_a.each do |error|
         Rails.logger.info "  - #{error}"
       end
