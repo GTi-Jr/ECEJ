62f79f24e89d64730888ea92a8791a8fa13503c4
CaioEPS <caioergos@gmail.com>
2016-01-12 04:25:10 -0300

    tried to use datatables

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,5 @@
 //= require jquery.maskedinput.min.js
 //= require crew/users.js
 //= require plugins/perfect-scrollbar/perfect-scrollbar.min.js
+//= require plugins/data-tables/js/jquery.dataTables.min.js
+//= require plugins/data-tables/data-tables-script.js
