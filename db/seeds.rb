5c7c0b098e1bd523b63fdb0a76147ab0cb415f5f
CaioEPS <caioergos@gmail.com>
2016-01-13 03:59:21 -0300

    Lots in seed edited to match new columns

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,11 +20,12 @@ lot1 = Lot.new do |lot|
   lot.name = "Primeiro lote"
   lot.number = 1
   lot.limit = 50
-  lot.value_not_federated = 385
-  lot.value_federated = 395
   lot.start_date = DateTime.new(2016, 1, 10, 0, 0, 0 , '-3')
   lot.end_date = DateTime.new(2016, 1, 11, 23, 59, 59 , '-3')
-  lot.payment_deadline = DateTime.new(2016, 1, 25, 23, 59, 59 , '-3')
+  lot.deadline_1 = DateTime.new(2016, 1, 25, 23, 59, 59 , '-3')
+  lot.deadline_2 = DateTime.new(2016, 2, 22, 23, 59, 59 , '-3')
+  lot.deadline_3 = DateTime.new(2016, 3, 21, 2, 59, 59 , '-3')
+  lot.deadline_4 = DateTime.new(2016, 4, 11, 23, 59, 59 , '-3')
 end
 lot1.save!
 
@@ -32,11 +33,12 @@ lot2 = Lot.new do |lot|
   lot.name = "Segundo lote"
   lot.number = 2
   lot.limit = 132
-  lot.value_not_federated = 395
-  lot.value_federated = 405
   lot.start_date = DateTime.new(2016, 1, 14, 0, 0, 0 , '-3')
   lot.end_date = DateTime.new(2016, 1, 16, 23, 59, 59 , '-3')
-  lot.payment_deadline = DateTime.new(2016, 1, 25, 23, 59, 59 , '-3')
+  lot.deadline_1 = DateTime.new(2016, 1, 25, 23, 59, 59 , '-3')
+  lot.deadline_2 = DateTime.new(2016, 2, 22, 23, 59, 59 , '-3')
+  lot.deadline_3 = DateTime.new(2016, 3, 21, 2, 59, 59 , '-3')
+  lot.deadline_4 = DateTime.new(2016, 4, 11, 23, 59, 59 , '-3')
 end
 lot2.save!
 
@@ -44,30 +46,11 @@ lot3 = Lot.new do |lot|
   lot.name = "Terceiro lote"
   lot.number = 3
   lot.limit = 100
-  lot.value_not_federated = 400
-  lot.value_federated = 410
   lot.start_date = DateTime.new(2016, 1, 21, 0, 0, 0 , '-3')
   lot.end_date = DateTime.new(2016, 7, 31, 23, 59, 59 , '-3')
-  lot.payment_deadline = DateTime.new(2016, 1, 31, 23, 59, 59 , '-3')
+  lot.deadline_1 = DateTime.new(2016, 2, 1, 23, 59, 59 , '-3')
+  lot.deadline_2 = DateTime.new(2016, 2, 29, 23, 59, 59 , '-3')
+  lot.deadline_3 = DateTime.new(2016, 3, 28, 2, 59, 59 , '-3')
+  lot.deadline_4 = DateTime.new(2016, 4, 18, 23, 59, 59 , '-3')
 end
 lot3.save!
-
-user = User.new do |user|
-  user.name = "Caio EPS"
-  user.email = "caioergos@hotmail.com"
-  user.junior_enterprise = "GTi Jr."
-  user.general_register = "2019019111517"
-  user.cpf = "604.808.474-25"
-  user.phone = "85997842824"
-  user.gender = "Masculino"
-  user.completed = true
-  user.paid_on = nil
-  user.payment_method = nil
-  user.payment_status = "Não processado"
-  user.birthday = DateTime.now - 20.years
-  user.active = true
-  user.password = "caiocaio"
-  user.password_confirmation = "caiocaio"
-  user.confirmed_at = DateTime.now
-end
-user.save!
