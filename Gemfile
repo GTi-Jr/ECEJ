bdda5816066d08543ba94cdb9c3903fd449ae8e2
caioeps <caioergos@gmail.com>
2016-02-27 20:34:16 -0300

    hotel model functions

--- Gemfile
+++ Gemfile
@@ -73,6 +73,8 @@ group :development, :test do
   gem 'byebug'
   # Use sqlite3 as the database for Active Record
   gem 'sqlite3'
+  # Testing
+  gem 'rspec-rails', '~> 3.0'
 end
 
 group :development do
--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -5,4 +5,29 @@ class Hotel < ActiveRecord::Base
 
 	has_many :rooms
 
+	def capacity
+		capacity = 0
+		rooms.all.each do |room|
+			capacity += room.capacity
+		end
+		capacity
+	end
+
+	def people
+		people = []
+		rooms.each do |room|
+			room.users.each do |user|
+				people << user
+			end
+		end
+		people
+	end
+
+	def number_of_people
+		number_of_people = 0
+		rooms.each do |room|
+			number_of_people += room.users.count
+		end
+		number_of_people
+	end
 end
