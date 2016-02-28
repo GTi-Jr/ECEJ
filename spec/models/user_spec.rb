0960a3506fbc29c114b7c331bb718954c17a5be7
caioeps <caioergos@gmail.com>
2016-02-27 23:37:37 -0300

    rspec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,33 @@
+require "rails_helper"
+
+RSpec.describe User, type: :model do
+	it "user can't go to concurrent events" do
+		user = User.create! do |user|
+			user.name         = "name"
+			user.email        = "email@email.com"
+			user.password     = "password"
+			user.lot_id       = 1
+			user.confirmed_at = DateTime.now
+		end
+
+		event_1 = Event.create! do |event|
+			event.name        = "Evento 1"
+			event.facilitator = "fac 1"
+			event.limit       = 50
+			event.start       = DateTime.now
+			event.end         = DateTime.now + 2.hours
+		end
+
+		event_2 = Event.create! do |event|
+			event.name        = "Evento 2"
+			event.facilitator = "fac 2"
+			event.limit       = 40
+			event.start       = DateTime.now - 1.hour
+			event.end         = DateTime.now + 1.hours
+		end
+
+		user.events << event_1
+
+		expect(user.has_concurrent_event? event_2).to eq(true)
+	end
+end
