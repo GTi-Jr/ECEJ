1cafc34700e99259885dbf14a252970f7f9e3f5f
caioeps <caioergos@gmail.com>
2016-03-10 21:51:33 -0300

    reordered functions and added some comments

--- app/models/event.rb
+++ app/models/event.rb
@@ -14,28 +14,23 @@ class Event < ActiveRecord::Base
   has_many :subscriptions
   has_many :users, through: :subscriptions
 
-  def start_must_be_smaller_than_end
-    errors.add(:start_time, "deve ser menor que a data de término") if self.start_time > self.end_time
-  end
-
-  def is_happening_now?
-    now = DateTime.now
-    now > self.start_time && now < self.end
-  end
-
+  # Returns all past events
   def self.past
     Event.select { |event| DateTime.now > event.end_time }
   end
 
+  # Returns all future events
   def self.future
     Event.select { |event| DateTime.now < event.start_time }
   end
 
+  # Returns all events tha are happening at the moment
   def self.happening
     now = DateTime.now
     Event.select { |event| now < event.end_time && now > event.start_time } 
   end
 
+  # Return the all dates that have, at least, one event with no duplicates and ordered
   def self.days
     days = []
     Event.all.each do |event|
@@ -45,4 +40,29 @@ class Event < ActiveRecord::Base
     days.sort
   end
 
+  # Add a user to the event
+  def add(user)
+    users << user
+  end
+
+  # Remove the user from the event
+  def remove(user)
+    users.delete user
+  end
+
+  # Checks if the event is full
+  def full?
+    users.count > limit
+  end
+
+  # Checks if the events is happening at the moment
+  def is_happening_now?
+    now = DateTime.now
+    now > self.start_time && now < self.end
+  end
+
+  # Validator method
+  def start_must_be_smaller_than_end
+    errors.add(:start_time, "deve ser menor que a data de término") if self.start_time > self.end_time
+  end
 end
