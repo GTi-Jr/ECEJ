895d52cb0a31bfda81d164b92e315c5647efd154
caioeps <caioergos@gmail.com>
2016-08-01 11:10:29 -0300

    layout dos quartos

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,2 +1,17 @@
 module RoomsHelper
+  def show_capacity(room)
+    people_count = room.users.count
+
+    if people_count < room.capacity
+      color = 'green-text darken-3'
+    else
+      color = 'red-text darken-3'
+    end
+
+    "
+      <span class=\"#{color}\">
+        #{people_count}/#{room.capacity}
+      </span>
+    ".html_safe
+  end
 end
