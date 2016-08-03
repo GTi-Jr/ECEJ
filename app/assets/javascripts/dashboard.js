c0e7e9e0e9d8ba1028539495f51722ddaa8b5c09
caioeps <caioergos@gmail.com>
2016-08-03 08:56:49 -0300

    identação e adição de função para o collapsible

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -33,81 +33,86 @@ $('.dropify').dropify({
     messages: {
         'default': 'Arraste a foto ou clique para enviar',
         'replace': 'Arraste ou clique para substituir',
-        'remove':  'Remover',
-        'error':   'Esse arquivo é muito grande'
+        'remove': 'Remover',
+        'error': 'Esse arquivo é muito grande'
 
     }
 });
 $('.dropdown-button').dropdown({
-      inDuration: 300,
-      outDuration: 225,
-      constrain_width: false, // Does not change width of dropdown to that of the activator
-      hover: true, // Activate on hover
-      gutter: 0, // Spacing from edge
-      belowOrigin: true, // Displays dropdown below the button
-      alignment: 'left' // Displays dropdown with edge aligned to the left of button
-    }
-  );
+    inDuration: 300,
+    outDuration: 225,
+    constrain_width: false, // Does not change width of dropdown to that of the activator
+    hover: true, // Activate on hover
+    gutter: 0, // Spacing from edge
+    belowOrigin: true, // Displays dropdown below the button
+    alignment: 'left' // Displays dropdown with edge aligned to the left of button
+});
 $('.progress').hide();
-if($("#edit_user").length){
-  $("#edit_user").validate({
-    submitHandler: function(form) {
-      // do other things for a valid form
-      $('.progress').show();
-      form.submit();
-    },
-    errorElement : 'div',
-    errorPlacement: function(error, element) {
-      var placement = $(element).data('error');
-      if (placement) {
-        $(placement).append(error)
-      } else {
-        error.insertAfter(element);
-        alert("Erro ao enviar imagem, permitidos apenas arquivos .jpg ou .png");
-      }
-    }
-   });
-   $( "#user_avatar" ).rules( "add", {
-     extension: "png|jpg",
-     messages: {
-       extension: "Só são permitidos arquivos .jpg ou .png"
-     }
-   });
+if ($("#edit_user").length) {
+    $("#edit_user").validate({
+        submitHandler: function(form) {
+            // do other things for a valid form
+            $('.progress').show();
+            form.submit();
+        },
+        errorElement: 'div',
+        errorPlacement: function(error, element) {
+            var placement = $(element).data('error');
+            if (placement) {
+                $(placement).append(error)
+            } else {
+                error.insertAfter(element);
+                alert("Erro ao enviar imagem, permitidos apenas arquivos .jpg ou .png");
+            }
+        }
+    });
+    $("#user_avatar").rules("add", {
+        extension: "png|jpg",
+        messages: {
+            extension: "Só são permitidos arquivos .jpg ou .png"
+        }
+    });
 }
-if($("#edit_password_form").length){
-   $("#edit_password_form").validate({
-     errorElement : 'div',
-     errorPlacement: function(error, element) {
-       var placement = $(element).data('error');
-       if (placement) {
-         $(placement).append(error)
-       } else {
-         error.insertAfter(element);
-       }
-     }
-   });
-   $( "#current_password" ).rules( "add", {
-     required: true,
-     messages: {
-       required: "Entre com sua senha atual",
-     }
-   });
-   $( "#new_password" ).rules( "add", {
-     required: true,
-     rangelength: [8,12],
-     messages: {
-       required: "Entre com sua senha",
-       rangelength: "Sua nova senha deve conter entre 8 e 12 caracteres"
-     }
-   });
-   $( "#new_confirm_password" ).rules( "add", {
-     required: true,
-     rangelength: [8,12],
-     equalTo: "#new_password",
-     messages: {
-       required: "Entre com sua senha",
-       rangelength: "Sua nova senha deve conter entre 8 e 12 caracteres",
-       equalTo: "As senhas não conferem"
-     }
-   });
- }
+if ($("#edit_password_form").length) {
+    $("#edit_password_form").validate({
+        errorElement: 'div',
+        errorPlacement: function(error, element) {
+            var placement = $(element).data('error');
+            if (placement) {
+                $(placement).append(error)
+            } else {
+                error.insertAfter(element);
+            }
+        }
+    });
+    $("#current_password").rules("add", {
+        required: true,
+        messages: {
+            required: "Entre com sua senha atual",
+        }
+    });
+    $("#new_password").rules("add", {
+        required: true,
+        rangelength: [8, 12],
+        messages: {
+            required: "Entre com sua senha",
+            rangelength: "Sua nova senha deve conter entre 8 e 12 caracteres"
+        }
+    });
+    $("#new_confirm_password").rules("add", {
+        required: true,
+        rangelength: [8, 12],
+        equalTo: "#new_password",
+        messages: {
+            required: "Entre com sua senha",
+            rangelength: "Sua nova senha deve conter entre 8 e 12 caracteres",
+            equalTo: "As senhas não conferem"
+        }
+    });
+}
+
+$(document).ready(function() {
+    $('.collapsible').collapsible({
+        accordion: false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+});
