6ba8e2f7b45f00d6aefe796bbdf524e6c52a3f03
caioeps <caioergos@gmail.com>
2016-08-15 14:09:06 -0300

    seed do admin e mudança da senha no email

--- db/seeds/add_almej_users.rb
+++ db/seeds/add_almej_users.rb
@@ -6,8 +6,16 @@ people.each do |row|
   user = admin.new_user( { name: row[0], email: row[1] } )
 
   if user.save
-    Rails.logger.info "#{user.name} foi adicionado"
+    p "#{user.name} foi adicionado"
   else
-    Rails.logger.info "Não foi possível adicionar #{user.email}. Erros:\n #{user.errors.full_messages}"
+    p "Não foi possível adicionar #{user.email}. Erros:\n #{user.errors.full_messages}"
   end
+end
+
+admin = Crew::Admin.new(email: 'admin@sistemaalmej.com', password: 'sistemaalmej2016')
+
+if admin.save
+  p 'admin salvado'
+else
+  p "Não foi possível adicionar #{admin.email}. Erros:\n #{admin.errors.full_messages}"
 end
