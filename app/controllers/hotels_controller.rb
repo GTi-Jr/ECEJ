3beee46eef9feea9b87e62078c76c43a9dad42c6
Caio Ergos <caioergos@gmail.com>
2016-09-07 22:13:14 -0300

    lol

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -2,12 +2,16 @@ class HotelsController < ApplicationController
 	before_action :authenticate_user!, :get_user
 	before_action :verify_register_conclusion
 	before_action :user_must_have_paid
-
+	before_action :eita
 	layout "dashboard"
 
 	# GET /hotels
 	# Lists all hotels ordered by name
 	def index
 		@hotels = Hotel.order(:name)
+	end
+
+	def eita
+	  redirect_to authenticated_user_root_path
 	end
 end
